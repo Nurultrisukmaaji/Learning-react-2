38e6e1a9f7e7c7e00bae7f70e89336440f04e0e1
Nurultrisukmaaji <nurultrisukmaaji@gmail.com>
2025-06-24 15:51:30 +0700

    update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react"
+
 export default function Header() {
+
+    const [isActive, setIsActive] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+
+    const toggleClass = () => {
+        setIsActive(!isActive);
+    }
+
     return(
-        <h1 className="first-header">Header</h1>
+        <>
+            <h1>Header</h1>
+            <div className={`mb-5 d-flex ${isActive ? 'box box-active' : 'box'}`}>
+                <div className="m-auto">Tes Box</div>
+            </div>
+            <button onClick={() => toggleClass()} className="btn btn-primary">Change Box</button>
+
+            <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className={`mt-5 d-flex ${isHovered ? 'box box-active' : 'box'}`}>
+                <div className="m-auto">{isHovered? 'Hover Box' : 'Box'}</div>
+            </div>
+           
+        </>
     )
 }
