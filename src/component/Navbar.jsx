d95a298130ebcf10b9a641d6911f19a0c3e3fa29
Nurultrisukmaaji <nurultrisukmaaji@gmail.com>
2025-06-26 14:37:27 +0700

    update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,9 +6,9 @@ export default function Navbar(){
 
     return(
         <>
-            <nav className="navbar navbar-expand-lg bg-white">
+            <nav className="navbar navbar-expand-lg bg-white fixed-top">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">D`CoffeeShop</Link>
+                    <Link className="navbar-brand" to="/">S`Coffee</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
--- src/component/story.jsx
+++ src/component/story.jsx
@@ -1,6 +1,26 @@
 import imgStory from '../assets/images/story-img.jpg'
 
 export default function Story(){
+
+    function TextStory(){
+        return(
+            <>
+                 <p>
+                    S`Coffee didirikan pada tahun 2022 dengan satu tujuan sederhana: 
+                    menghadirkan pengalaman ngopi yang hangat, autentik, dan penuh rasa. 
+                    Berawal dari sudut kecil kota, kami percaya bahwa secangkir kopi yang 
+                    diracik dengan hati bisa menjadi awal dari percakapan, ide, 
+                    dan persahabatan yang luar biasa. <br /><br/> Kami menggabungkan biji 
+                    kopi lokal pilihan, metode seduh modern, dan suasana nyaman 
+                    untuk menciptakan tempat yang lebih dari sekadar coffee shop — tempat di mana 
+                    setiap orang bisa merasa seperti di rumah. Seiring waktu, D`CoffeeShop terus berkembang. 
+                    Tapi semangat kami tetap sama: menyajikan kopi terbaik dan menghadirkan 
+                    momen yang bermakna untuk setiap pelanggan yang datang. <br /> <b> S`Coffee – More Than Just Coffee.</b>
+                </p>
+            </>
+        )
+    }
+
     return(
         <>
             <div className="story-wrap-content">
@@ -13,18 +33,12 @@ export default function Story(){
                             </div>
                         </div>
                         <div className="col-sm-12 col-md-12 col-lg-6">
-                           <p>
-                            D`CoffeeShop didirikan pada tahun 2022 dengan satu tujuan sederhana: 
-                            menghadirkan pengalaman ngopi yang hangat, autentik, dan penuh rasa. 
-                            Berawal dari sudut kecil kota, kami percaya bahwa secangkir kopi yang 
-                            diracik dengan hati bisa menjadi awal dari percakapan, ide, 
-                            dan persahabatan yang luar biasa. Kami menggabungkan biji 
-                            kopi lokal pilihan, metode seduh modern, dan suasana nyaman 
-                            untuk menciptakan tempat yang lebih dari sekadar coffee shop — tempat di mana 
-                            setiap orang bisa merasa seperti di rumah. Seiring waktu, D`CoffeeShop terus berkembang. 
-                            Tapi semangat kami tetap sama: menyajikan kopi terbaik dan menghadirkan 
-                            momen yang bermakna untuk setiap pelanggan yang datang. <br /> <b> D`CoffeeShop – More Than Just Coffee.</b>
-                           </p>
+                            <div className="d-flex flex-column h-100">
+                                <TextStory />
+                                <div className='mt-auto'>
+                                    <button className='btn btn-detail shadow mt-4'>Selengkapnya <i className="bi bi-arrow-right"></i></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
